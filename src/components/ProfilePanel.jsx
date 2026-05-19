c91f1bec3800cf661ecc3efeb89d25c9109af359
sherinesivy <112262609+sherinesivy@users.noreply.github.com>
2026-05-19 19:42:33 +0530

    Update ProfilePanel.jsx

--- src/components/ProfilePanel.jsx
+++ src/components/ProfilePanel.jsx
@@ -43,7 +43,7 @@ function ProfilePanel() {
 
       {/* Name + Verified */}
       <div className="profileName">
-        <span>Irshath Sherine S S</span>
+        <span>Irshathsherine S S</span>
         <span className="verifiedBadge">✔</span>
       </div>
 
